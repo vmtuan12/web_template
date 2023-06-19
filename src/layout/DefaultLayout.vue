756d56f1ef3d6a398c3b22f29b1b4dc8d3102de9
vmtuan12 <21020664@vnu.edu.vn>
2023-06-19 15:56:30 +0700

    add profile dropdown to navbar

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -1,10 +1,17 @@
 <script setup>
 import Navbar from '../components/UI/Navbar.vue';
+import ProfileDropdown from '../components/UI/ProfileDropdown.vue'
 </script>
 
 <template>
     <div class="flex w-full h-full">
         <Navbar />
-        <router-view />
+        <div class="sm:w-[calc(100%-288px)]">
+            <div class="bg-[#f5f7fb] flex w-full items-center justify-between py-1 px-2 text-[#52a1f5]">
+                <p class="text-xl font-semibold">Dashboard</p>
+                <ProfileDropdown />
+            </div>
+            <router-view />
+        </div>
     </div>
 </template>
