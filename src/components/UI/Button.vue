40d983eb9a02e7f79c33f3d9c19e329032af2bb9
vmtuan12 <21020664@vnu.edu.vn>
2023-06-21 10:42:00 +0700

    adjust padding, more border radius type

--- src/components/UI/Button.vue
+++ src/components/UI/Button.vue
@@ -9,15 +9,17 @@ const round = ref({
     md: 'rounded-md',
     lg: 'rounded-lg',
     xl: 'rounded-xl',
+    xxl: 'rounded-2xl',
+    xxxl: 'rounded-3xl',
     full: 'rounded-full'
 })
 
 const colors = ref({
     primary: 'bg-[#52a1f5] hover:bg-[#3d96f5] active:bg-[#2d8ef7] text-white',
     success: 'bg-[#39c0c8] hover:bg-[#2eccd4] active:bg-[#23d0d9] text-white',
-    danger: 'bg-[#f34971] hover:bg-[#ff3564] active:bg-[#f71b4e]',
-    neon: 'bg-[#6c47ff] hover:bg-[#5e36ff] active:bg-[#5f37ff]',
-    orange: 'bg-[#f96300] hover:bg-[#ff781d] active:bg-[#fe7113]'
+    danger: 'bg-[#f34971] hover:bg-[#ff3564] active:bg-[#f71b4e] text-white',
+    neon: 'bg-[#6c47ff] hover:bg-[#5e36ff] active:bg-[#5f37ff] text-white',
+    orange: 'bg-[#f96300] hover:bg-[#ff781d] active:bg-[#fe7113] text-white'
 });
 
 const weight = ref({
@@ -32,7 +34,7 @@ const weight = ref({
 <template>
     <button
         @click="$emit('click')"
-        class="px-2 py-1 text-white transition-all duration-200 ease-in-out flex items-center gap-1 hover:[transform:translateY(-2px)]"
+        class="px-3 py-1.5 text-white transition-all duration-200 ease-in-out flex items-center gap-1 hover:[transform:translateY(-2px)]"
         :class="colors[bgColor] !== undefined ? colors[bgColor] : colors['primary'], round[borderRadius] !== undefined ? round[borderRadius] : round['md'], weight[fontWeight] !== undefined ? weight[fontWeight] : weight['normal']"
     >
         <slot></slot>
